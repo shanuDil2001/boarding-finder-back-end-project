0ccc8cfb253944ecda6a88a2a189b39bbfe6f0c5
“shanuDil2001” <shanukadilshan.swe@gmail.com>
2026-05-08 11:04:31 +0530

    Defined a schema for boarding houses ✏️

---
+++ models/boarding.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const boardingSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 20,
+    },
+    availableRooms: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    images: {
+      type: [String],
+      required: true,
+
+      validate: {
+        validator: (value) => value.length > 0,
+        message: "At least one image is required",
+      },
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+        validate: {
+          validator: (value) => value.length === 2,
+          message: "Coordinates must contain longitude and latitude",
+        },
+      },
+    },
+    university: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "universities",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+boardingSchema.index({ location: "2dsphere" });
+
+const Boarding = mongoose.model("boardings", boardingSchema);
+
+export default Boarding;
